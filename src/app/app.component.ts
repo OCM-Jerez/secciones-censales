78ce784d9925414811a1a573e972c6ce5e49d86a
mamjerez <correomam@gmail.com>
2023-04-20 19:34:32 +0200

    Add 01-001 to 01-005

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { Map, polygon, tileLayer } from 'leaflet';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Icon, Map, marker, polygon, tileLayer } from 'leaflet';
+
+import { ds01001 } from '../assets/data/01-001';
+import { ds01002 } from '../assets/data/01-002';
+import { ds01003 } from '../assets/data/01-003';
+import { ds01004 } from '../assets/data/01-004';
+import { ds01005 } from '../assets/data/01-005';
 
 import { ds02004 } from '../assets/data/02-004';
 import { ds02011 } from '../assets/data/02-011';
@@ -13,12 +19,78 @@ import { ds02026 } from '../assets/data/02-026';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
   constructor() {}
+
+  ngOnInit(): void {
+    const initialArray = [];
+    // const ds02003 = initialArray.map(([lng, lat]) => ({ lng, lat }));
+    // const tsContent = `export const ds01001 = ${JSON.stringify(
+    //   ds02003,
+    //   null,
+    //   4
+    // )};\n`;
+    // console.log(tsContent);
+
+    // const o = initialArray.map(([lng, lat]) => ({ lat, lng }));
+    // console.log(o);
+  }
 
   ngAfterViewInit(): void {
     // http://leaflet-extras.github.io/leaflet-providers/preview/
     const mapProblemas = new Map('map');
+
+    const ds01001Array: [number, number][] = [];
+    ds01001.forEach((element) => {
+      ds01001Array.push([element.lat, element.lng]);
+    });
+    polygon(ds01001Array, {
+      color: 'red',
+      fillColor: 'red',
+      fillOpacity: 0.1,
+    }).addTo(mapProblemas);
+
+    let Array: [number, number][] = [];
+    ds01002.forEach((element) => {
+      Array.push([element.lat, element.lng]);
+    });
+    polygon(Array, {
+      color: 'red',
+      fillColor: 'red',
+      fillOpacity: 0.1,
+    }).addTo(mapProblemas);
+
+    let Array01003: [number, number][] = [];
+    ds01003.forEach((element) => {
+      Array01003.push([element.lat, element.lng]);
+    });
+    polygon(Array01003, {
+      color: 'red',
+      fillColor: 'red',
+      fillOpacity: 0.1,
+    }).addTo(mapProblemas);
+
+    let Array01004: [number, number][] = [];
+    ds01004.forEach((element) => {
+      Array01004.push([element.lat, element.lng]);
+    });
+    polygon(Array01004, {
+      color: 'red',
+      fillColor: 'red',
+      fillOpacity: 0.1,
+    }).addTo(mapProblemas);
+
+    let Array01005: [number, number][] = [];
+    ds01005.forEach((element) => {
+      Array01005.push([element.lat, element.lng]);
+    });
+    polygon(Array01005, {
+      color: 'red',
+      fillColor: 'red',
+      fillOpacity: 0.1,
+    }).addTo(mapProblemas);
+
+    // ================================================================================================
 
     const ds02021Array: [number, number][] = [];
     ds02021.forEach((element) => {
@@ -88,6 +160,57 @@ export class AppComponent implements AfterViewInit {
 
     mapProblemas.setView([36.68519, -6.13229], 15);
 
+    const ideaIcon = new Icon({
+      iconUrl:
+        'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+      shadowUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41],
+      // shadowAnchor: [22, 94]
+    });
+
+    const ideas = [
+      {
+        title: `<h1>Sección censal 02-0004</h1>
+          <h3>Datos</h3>
+  
+          <img src="https://res.cloudinary.com/dabrencx7/image/upload/v1647547099/albarizuela/fotosRutas/Caldereros_sxrbgn.jpg" alt="Don Juan" width="100px">
+         `,
+        lat: 36.68528,
+        long: -6.13279,
+      },
+      {
+        title: ``,
+        lat: 36,
+        long: -6,
+      },
+    ];
+
+    ideas.map((point) => {
+      marker([point.lat, point.long], {
+        icon: ideaIcon,
+      })
+        .addTo(mapProblemas)
+        .bindPopup(point.title);
+    });
+
     var overlays = {};
+
+    // const text = 'Hello, World!';
+    // const icon = divIcon({
+    //   className: 'custom-text-marker',
+    //   html: `<div>${text}</div>`,
+    // });
+
+    // const coordinates = [-6.13279, 36.68528];
+    // // const coordinates = [36.68528, , -6.13279];
+    // marker([coordinates], {
+    //   icon: ideaIcon,
+    // })
+    //   .addTo(mapProblemas)
+    //   .bindPopup(point.title);
   }
 }
