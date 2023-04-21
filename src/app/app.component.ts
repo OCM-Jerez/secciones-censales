f09e3cd32e6e719c4cd68bfe694f4cfbbccfa5d9
mamjerez <correomam@gmail.com>
2023-04-21 05:58:11 +0200

    Add seccionesCensales.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { Icon, Map, marker, polygon, tileLayer } from 'leaflet';
+// import { Icon, map, marker, polygon, tileLayer } from 'leaflet';
+import { Icon, geoJSON, map, marker, polygon, tileLayer } from 'leaflet';
+
+import { GeoJsonObject } from 'geojson';
+import distritos from '../assets/data/secionesCensales.json';
 
 import { ds01001 } from '../assets/data/01-001';
 import { ds01002 } from '../assets/data/01-002';
@@ -20,6 +24,36 @@ import { ds02026 } from '../assets/data/02-026';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements AfterViewInit, OnInit {
+  geoJson: GeoJsonObject | undefined;
+  // Configuración del mapa base.
+  tileLayerConfig = {
+    maxZoom: 17,
+    minZoom: 10,
+    attribution:
+      'Map data &copy; <a href="http://openstreetmap.org"> OpenStreetMap</a> Contributors, ' +
+      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.light',
+  };
+
+  onEachFeature = (feature, layer) => {
+    layer.on({
+      mouseover: this.highlightFeature,
+      // mouseout: this.resetHighlight,
+      // click: this.zoomToFeature
+    });
+  };
+
+  highlightFeature = (e) => {
+    const layer = e.target;
+    layer.setStyle({
+      weight: 5,
+      color: '#666',
+      dashArray: '',
+      fillOpacity: 0.5,
+    });
+  };
+
   constructor() {}
 
   ngOnInit(): void {
@@ -38,127 +72,30 @@ export class AppComponent implements AfterViewInit, OnInit {
 
   ngAfterViewInit(): void {
     // http://leaflet-extras.github.io/leaflet-providers/preview/
-    const mapProblemas = new Map('map');
-
-    const ds01001Array: [number, number][] = [];
-    ds01001.forEach((element) => {
-      ds01001Array.push([element.lat, element.lng]);
+    // const mamOCM = new Map('map');
+    const mamOCM = map('map', {
+      // center: [36.686881, -6.142603],
+      zoomControl: false,
+      zoom: 14,
+      maxBounds: [
+        // Esquina superior izquierda hasta donde permitira mover.
+        [36.534027, -6.332731],
+        // Esquina inferior derecha.
+        [36.819779, -5.40752],
+      ],
     });
-    polygon(ds01001Array, {
-      color: 'red',
-      fillColor: 'red',
-      fillOpacity: 0.1,
-    }).addTo(mapProblemas);
 
-    let Array: [number, number][] = [];
-    ds01002.forEach((element) => {
-      Array.push([element.lat, element.lng]);
-    });
-    polygon(Array, {
-      color: 'red',
-      fillColor: 'red',
-      fillOpacity: 0.1,
-    }).addTo(mapProblemas);
+    tileLayer(
+      'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      this.tileLayerConfig
+    ).addTo(mamOCM);
 
-    let Array01003: [number, number][] = [];
-    ds01003.forEach((element) => {
-      Array01003.push([element.lat, element.lng]);
-    });
-    polygon(Array01003, {
-      color: 'red',
-      fillColor: 'red',
-      fillOpacity: 0.1,
-    }).addTo(mapProblemas);
+    mamOCM.setView([36.68519, -6.13229], 15);
 
-    let Array01004: [number, number][] = [];
-    ds01004.forEach((element) => {
-      Array01004.push([element.lat, element.lng]);
-    });
-    polygon(Array01004, {
-      color: 'red',
-      fillColor: 'red',
-      fillOpacity: 0.1,
-    }).addTo(mapProblemas);
-
-    let Array01005: [number, number][] = [];
-    ds01005.forEach((element) => {
-      Array01005.push([element.lat, element.lng]);
-    });
-    polygon(Array01005, {
-      color: 'red',
-      fillColor: 'red',
-      fillOpacity: 0.1,
-    }).addTo(mapProblemas);
-
-    // ================================================================================================
-
-    const ds02021Array: [number, number][] = [];
-    ds02021.forEach((element) => {
-      ds02021Array.push([element.lat, element.long]);
-    });
-    polygon(ds02021Array, {
-      color: 'green',
-      fillColor: 'green',
-      fillOpacity: 0.1,
-    }).addTo(mapProblemas);
-
-    const ds02004Array: [number, number][] = [];
-    ds02004.forEach((element) => {
-      ds02004Array.push([element.lat, element.lng]);
-    });
-    polygon(ds02004Array, {
-      color: 'blue',
-      fillColor: 'blue',
-      fillOpacity: 0.2,
-    }).addTo(mapProblemas);
-
-    const ds02014Array: [number, number][] = [];
-    ds02014.forEach((element) => {
-      ds02014Array.push([element.lat, element.lng]);
-    });
-    polygon(ds02014Array, {
-      color: 'yellow',
-      fillColor: 'yellow',
-      fillOpacity: 0.2,
-    }).addTo(mapProblemas);
-
-    const ds02026Array: [number, number][] = [];
-    ds02026.forEach((element) => {
-      ds02026Array.push([element.lat, element.lng]);
-    });
-    polygon(ds02026Array, {
-      color: 'black',
-      fillColor: 'black',
-      fillOpacity: 0.2,
-    }).addTo(mapProblemas);
-
-    const ds02011Array: [number, number][] = [];
-    ds02011.forEach((element) => {
-      ds02011Array.push([element.lat, element.lng]);
-    });
-    polygon(ds02011Array, {
-      color: 'green',
-      fillColor: 'green',
-      fillOpacity: 0.1,
-    }).addTo(mapProblemas);
-
-    const ds02013Array: [number, number][] = [];
-    ds02013.forEach((element) => {
-      ds02013Array.push([element.lat, element.lng]);
-    });
-    polygon(ds02013Array, {
-      color: 'blue',
-      fillColor: 'blue',
-      fillOpacity: 0.2,
-    }).addTo(mapProblemas);
-
-    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(mapProblemas);
-
-    mapProblemas.setView([36.68519, -6.13229], 15);
+    const geoJson = geoJSON(distritos as GeoJsonObject, {
+      // style: this.style2(),
+      // onEachFeature: this.onEachFeature,
+    }).addTo(mamOCM);
 
     const ideaIcon = new Icon({
       iconUrl:
@@ -193,7 +130,7 @@ export class AppComponent implements AfterViewInit, OnInit {
       marker([point.lat, point.long], {
         icon: ideaIcon,
       })
-        .addTo(mapProblemas)
+        .addTo(mamOCM)
         .bindPopup(point.title);
     });
 
@@ -210,7 +147,123 @@ export class AppComponent implements AfterViewInit, OnInit {
     // marker([coordinates], {
     //   icon: ideaIcon,
     // })
-    //   .addTo(mapProblemas)
+    //   .addTo(mamOCM)
     //   .bindPopup(point.title);
+
+    function name(map) {
+      // ================================================================================================
+
+      const ds01001Array: [number, number][] = [];
+      ds01001.forEach((element) => {
+        ds01001Array.push([element.lat, element.lng]);
+      });
+      polygon(ds01001Array, {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 0.1,
+      }).addTo(mamOCM);
+
+      let Array: [number, number][] = [];
+      ds01002.forEach((element) => {
+        Array.push([element.lat, element.lng]);
+      });
+      polygon(Array, {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 0.1,
+      }).addTo(mamOCM);
+
+      let Array01003: [number, number][] = [];
+      ds01003.forEach((element) => {
+        Array01003.push([element.lat, element.lng]);
+      });
+      polygon(Array01003, {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 0.1,
+      }).addTo(mamOCM);
+
+      let Array01004: [number, number][] = [];
+      ds01004.forEach((element) => {
+        Array01004.push([element.lat, element.lng]);
+      });
+      polygon(Array01004, {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 0.1,
+      }).addTo(mamOCM);
+
+      let Array01005: [number, number][] = [];
+      ds01005.forEach((element) => {
+        Array01005.push([element.lat, element.lng]);
+      });
+      polygon(Array01005, {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 0.1,
+      }).addTo(mamOCM);
+
+      // ================================================================================================
+
+      const ds02021Array: [number, number][] = [];
+      ds02021.forEach((element) => {
+        ds02021Array.push([element.lat, element.long]);
+      });
+      polygon(ds02021Array, {
+        color: 'green',
+        fillColor: 'green',
+        fillOpacity: 0.1,
+      }).addTo(mamOCM);
+
+      const ds02004Array: [number, number][] = [];
+      ds02004.forEach((element) => {
+        ds02004Array.push([element.lat, element.lng]);
+      });
+      polygon(ds02004Array, {
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.2,
+      }).addTo(mamOCM);
+
+      const ds02014Array: [number, number][] = [];
+      ds02014.forEach((element) => {
+        ds02014Array.push([element.lat, element.lng]);
+      });
+      polygon(ds02014Array, {
+        color: 'yellow',
+        fillColor: 'yellow',
+        fillOpacity: 0.2,
+      }).addTo(mamOCM);
+
+      const ds02026Array: [number, number][] = [];
+      ds02026.forEach((element) => {
+        ds02026Array.push([element.lat, element.lng]);
+      });
+      polygon(ds02026Array, {
+        color: 'black',
+        fillColor: 'black',
+        fillOpacity: 0.2,
+      }).addTo(mamOCM);
+
+      const ds02011Array: [number, number][] = [];
+      ds02011.forEach((element) => {
+        ds02011Array.push([element.lat, element.lng]);
+      });
+      polygon(ds02011Array, {
+        color: 'green',
+        fillColor: 'green',
+        fillOpacity: 0.1,
+      }).addTo(mamOCM);
+
+      const ds02013Array: [number, number][] = [];
+      ds02013.forEach((element) => {
+        ds02013Array.push([element.lat, element.lng]);
+      });
+      polygon(ds02013Array, {
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.2,
+      }).addTo(mamOCM);
+    }
   }
 }
